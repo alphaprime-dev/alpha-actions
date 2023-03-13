730334828009a2acbf860198b771205e1bd39377
myeongjunkim <ilikerunning@naver.com>
2023-03-13 16:32:46 +0900

    chore: wip

--- scripts/notify-discord.py
+++ scripts/notify-discord.py
@@ -4,9 +4,10 @@ import click
 @click.command(help="CLI for sending message to discord.")
 @click.option("--repo", "-f", type=click.STRING, required=True)
 @click.option("--tag", "-f", type=click.STRING, required=True)
-def main(repo: str, tag: str) -> None:
+@click.option("--env", "-f", type=click.STRING, required=True)
+def main(env:str, repo: str, tag: str) -> None:
     webhook_url = "https://discord.com/api/webhooks/1062906330001723463/jFCyiTAA2Qi_427spFDTHQjQexN6d1Hwar63d2bz22xGDaBcwlm2pbHcwKnTtlw2_HxT" # noqa
-    discord_client = DiscordClient(webhook_url, repo, tag)
+    discord_client = DiscordClient(webhook_url, env, repo, tag)
     discord_client.send_message()
 
 class DiscordClient:
