1aea608f5670d93bbeb2fbaf9b203bbd17e9187d
myeongjunkim <ilikerunning@naver.com>
2023-03-13 14:58:10 +0900

    chore: wip

--- scripts/notify-discord.py
+++ scripts/notify-discord.py
@@ -1,11 +1,37 @@
 import requests
 
-
 url = "https://discord.com/api/webhooks/1062906330001723463/jFCyiTAA2Qi_427spFDTHQjQexN6d1Hwar63d2bz22xGDaBcwlm2pbHcwKnTtlw2_HxT"
 data = {
-    "content": "test github actions",
-    "embeds": None,
-    "attachments": []
+  "content": None,
+  "embeds": [
+    {
+      "title": "Release Completed",
+      "description": "---\n\n[alphaprime-dev/alphaprime-api-bot] version v0.5.1 image is pushed \n\n[Go to Argo CD](https://argocd.alphasquare.co.kr/applications/data-server-prod )\n\n---",
+      "color": 5795484,
+      "fields": [
+        {
+          "name": "Environment",
+          "value": "PROD",
+          "inline": True
+        },
+        {
+          "name": "Repository",
+          "value": "[alphaprime-api-bot](https://github.com/alphaprime-dev/alphasquare-main-server/tree/refs/tags/v23.1.0)",
+          "inline": True
+        },
+        {
+          "name": "Release Note",
+          "value": "[v0.5.1](https://github.com/alphaprime-dev/alphaprime-api-bot/releases/tag/v0.5.1)",
+          "inline": True
+        }
+      ],
+      "author": {
+        "name": "alphaprime-api-bot"
+      }
+    }
+  ],
+  "attachments": []
 }
+
 
 res = requests.post(url, json=data)
