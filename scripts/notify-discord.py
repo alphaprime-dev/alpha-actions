fec59493ddff31c719dfe93dac55ded37e20d01a
myeongjunkim <ilikerunning@naver.com>
2023-03-13 14:48:51 +0900

    chore: wip

--- scripts/notify-discord.py
+++ scripts/notify-discord.py
@@ -2,18 +2,17 @@ import urllib.request
 import urllib.parse
 
 
-print("Hello World")
-# url = "https://discord.com/api/webhooks/1062906330001723463/jFCyiTAA2Qi_427spFDTHQjQexN6d1Hwar63d2bz22xGDaBcwlm2pbHcwKnTtlw2_HxT"
-# data = {
-#     "content": "test github actions",
-#     "embeds": None,
-#     "attachments": []
-# }
+url = "https://discord.com/api/webhooks/1062906330001723463/jFCyiTAA2Qi_427spFDTHQjQexN6d1Hwar63d2bz22xGDaBcwlm2pbHcwKnTtlw2_HxT"
+data = {
+    "content": "test github actions",
+    "embeds": None,
+    "attachments": []
+}
 
-# req = urllib.request.Request(
-#     url=url, 
-#     data=urllib.parse.urlencode(data).encode("utf-8"), 
-#     method="POST"
-# )
+req = urllib.request.Request(
+    url=url, 
+    data=urllib.parse.urlencode(data).encode("utf-8"), 
+    method="POST"
+)
 
-# res = urllib.request.urlopen(req)
+res = urllib.request.urlopen(req)
