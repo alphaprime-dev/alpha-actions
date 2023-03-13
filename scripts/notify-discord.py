b503c8c3a0f1b82624c55333ecdae85a66435f38
myeongjunkim <ilikerunning@naver.com>
2023-03-13 20:11:23 +0900

    chore: add status options

--- scripts/notify-discord.py
+++ scripts/notify-discord.py
@@ -1,60 +1,64 @@
+from typing import Literal
 from discord_webhook import DiscordEmbed, DiscordWebhook
 import click
 
 @click.command(help="CLI for sending message to discord.")
-@click.option("--repo", "-f", type=click.STRING, required=True)
-@click.option("--tag", "-f", type=click.STRING, required=True)
-@click.option("--env", "-f", type=click.STRING, required=True)
-def main(env:str, repo: str, tag: str) -> None:
+@click.option("--status", "-s", type=click.STRING, required=True)
+@click.option("--env", "-e", type=click.STRING, required=True)
+@click.option("--repo", "-r", type=click.STRING, required=True)
+@click.option("--tag", "-t", type=click.STRING, required=True)
+def main(status:str, env:str, repo: str, tag: str) -> None:
     webhook_url = "https://discord.com/api/webhooks/1062906330001723463/jFCyiTAA2Qi_427spFDTHQjQexN6d1Hwar63d2bz22xGDaBcwlm2pbHcwKnTtlw2_HxT" # noqa
-    discord_client = DiscordClient(webhook_url, env, repo, tag)
+    discord_client = DiscordClient(webhook_url, status, env,  repo, tag)
     discord_client.send_message()
 
 class DiscordClient:
-    def __init__(self, webhook_url, env, repo, tag) -> None:
+    def __init__(self, webhook_url, status, env, repo, tag) -> None:
         self.webhook_url = webhook_url
-        self.env = env
-        self.repo = repo
-        self.tag = tag
+        self.status:Literal["true","false"] = status
+        self.env:str = env
+        self.repo:str = repo
+        self.tag:str = tag
     
     def send_message(self) -> None:
+        
         embed = self.create_embed(self.env, self.repo, self.tag)
         webhook = DiscordWebhook(self.webhook_url)
         webhook.add_embed(embed)
         webhook.execute()
 
-    def create_embed(self, env:str, repo: str, tag: str) -> DiscordEmbed:
+    def create_embed(self) -> DiscordEmbed:
+        status = "성공" if self.status == "true" else "실패"
         embed = DiscordEmbed(color="03b2f8")
-        embed.set_author(name=repo)
-        embed.set_title(title="🔔 릴리즈 완료 🔔")
-
-        repo_name = repo.split("/")[1]
+        embed.set_author(name=self.repo)
+        embed.set_title(title=f"🔔 릴리즈 {status} 🔔")
+        repo_name = self.repo.split("/")[1]
         embed.set_description(f"""
-        **{env}**
-        {repo_name} 의 {tag} 이미지가 빌드 & 푸쉬 되었습니다.
-        Deploy를 진행해주세요 ✈️
+        **{self.env}**
+        {repo_name} 의 {self.tag} 이미지 빌드 & 푸쉬가 {status}했습니다.
+        결과를 확인해 주세요 ✈️
         """)
 
         embed.add_embed_field(
             name="Deploy",
-            value=self._get_deploy_link(repo),
+            value=self._get_deploy_link(self.repo),
             inline=True
         )
         embed.add_embed_field(
             name="Release Note",
-            value=f"[{tag}](https://github.com/{repo}/releases/tag/{tag})",
+            value=f"[{self.tag}](https://github.com/{self.repo}/releases/tag/{self.tag})",
             inline=True
         )
         embed.add_embed_field(
             name="Actions Workflow",
-            value=f"[{repo_name}](https://github.com/{repo}/actions)",
+            value=f"[{repo_name}](https://github.com/{self.repo}/actions)",
             inline=True
         )
         return embed
 
 
     def _get_deploy_link(self, repo: str) -> str:
-        if repo == "alphacrawler":
+        if repo == "alphaprime-dev/alphacrawler":
             return "[Airflow](https://airflow.alphasquare.co.kr/variable/list)"
         ENDPOINT = {
             "alphaprime-dev/alphasquare-main-server": "main-server-prod",
