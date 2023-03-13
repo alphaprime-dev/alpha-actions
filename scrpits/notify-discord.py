4e221ae8857fdafc7ba4b69e62060b2ff3b89f2f
myeongjunkim <ilikerunning@naver.com>
2023-03-13 14:44:18 +0900

    chore: wip

---
+++ scrpits/notify-discord.py
@@ -0,0 +1,19 @@
+import urllib.request
+import urllib.parse
+
+
+print("Hello World")
+# url = "https://discord.com/api/webhooks/1062906330001723463/jFCyiTAA2Qi_427spFDTHQjQexN6d1Hwar63d2bz22xGDaBcwlm2pbHcwKnTtlw2_HxT"
+# data = {
+#     "content": "test github actions",
+#     "embeds": None,
+#     "attachments": []
+# }
+
+# req = urllib.request.Request(
+#     url=url, 
+#     data=urllib.parse.urlencode(data).encode("utf-8"), 
+#     method="POST"
+# )
+
+# res = urllib.request.urlopen(req)
